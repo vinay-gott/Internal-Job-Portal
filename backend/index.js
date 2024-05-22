412133bdeb7fea3e8a5bc9e38c40a81408bf4e6b
SaiDeekshith <nethisaideekshith@gmail.com>
2024-05-22 17:17:11 +0530

    config,appliedjob.model,.env updated

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,32 @@
-const express=require("express")
-var app=express()
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const DBconnection = require('./config/db');
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Home page");
+dotenv.config();
+DBconnection();
 
-})
+// Import route files
+const loginRoute = require('./routes/LoginRoute.route');
+const jobRoute = require('./routes/JobRoute.route');
+const employeeRoute = require('./routes/EmployeeRoute.route');
+const appliedJobRoute = require('./routes/AppliedJobRoute.route');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+// Set base endpoint for all routes
+app.use('/api/login', loginRoute);
+app.use('/api/job', jobRoute);
+app.use('/api/employee', employeeRoute);
+app.use('/api/appliedjob', appliedJobRoute)
 
 
-app.listen(3128,()=>{
-    console.log("Listenig to port 3128")
-})
+const PORT = process.env.PORT || 3128;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
